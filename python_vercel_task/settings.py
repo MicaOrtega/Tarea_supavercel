3fdc2a478b332d9bd7bc6e98a32ff8e0e11f4cf6
MicaOrtega <micaelaortega13@gmail.com>
2024-11-25 10:37:09 -0300

    Inicio

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -1,3 +1,5 @@
+from decouple import  config
+from os import path
 """
 Django settings for python_vercel_task project.
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -68,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'python_vercel_task.wsgi.application'
+WSGI_APPLICATION = 'python_vercel_task.wsgi.app'
 
 
 # Database
@@ -77,11 +79,11 @@ WSGI_APPLICATION = 'python_vercel_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'python_vercel_task',
-        'USER': 'sample_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres.phdwtpllihidbvwadkyr',
+        'HOST': config('SUPABASE_HOST'),
+        'PASSWORD': config('SUPABASE_PASSWORD'),
+        'PORT': '6543',
     }
 }
 
