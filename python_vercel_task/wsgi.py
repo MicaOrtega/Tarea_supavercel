c070d7b2d35e6082c972d98b1e77d35cfbf4a809
MicaOrtega <micaelaortega13@gmail.com>
2024-11-25 17:59:01 -0300

    Actual

--- python_vercel_task/wsgi.py
+++ python_vercel_task/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_app
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_vercel_task.settings')
 
-app = python_vercel_task()
+application = get_wsgi_application()
